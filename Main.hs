b9a7c6aa9e2446876bd23044a8d1b75001a0a2fa
Ben B <bburdette@gmail.com>
2015-07-22 08:59:44 -0700

    toNote ftn

--- Main.hs
+++ Main.hs
@@ -5,6 +5,12 @@ import System.Environment
 import qualified Sound.MIDI.File as MFile
 import qualified Sound.MIDI.File.Event as Event
 import qualified Data.EventList.Relative.TimeBody as EventList
+import qualified Sound.MIDI.Message as MMessage
+
+import qualified Sound.MIDI.Message.Channel       as ChannelMsg
+import qualified Sound.MIDI.Message.Channel.Voice as Voice
+
+
 
 main = do
   args <- getArgs
@@ -22,9 +28,30 @@ loadIt fp = do
   let events = MFile.mergeTracks tp tracks
   print $ (length tracks)
   print (EventList.duration events)
-  EventList.mapBodyM print events
+  -- EventList.mapBodyM print events
+  -- mapM print (EventList.toPairList events)
+  
+  mapM (\(a,b) -> print (toNote b)) (EventList.toPairList events)
   return ()
 
--- printevts  
+toNote evt = 
+  case evt of 
+    Event.MIDIEvent blah -> 
+      let wha = ChannelMsg.messageBody blah in 
+      case wha of 
+        ChannelMsg.Voice (Voice.NoteOn p v) -> Just (Voice.fromPitch p)
+        _ -> Nothing
+      -- Just wha
+    _ -> Nothing
+      
+{-
 
+toNoteClusters :: [(time, body)] 
+
+toNoteClusters (ev:evs) = 
+  foldrPair blah 
+
+-}
+
+-- printevts  
 preent mf = (\(MFile.Cons tp div traks) -> show (length traks)) mf

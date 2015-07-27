1b256bc33a2cc6df05e7c8086eebe80fe311abe3
Ben B <bburdette@gmail.com>
2015-07-27 10:39:23 -0700

    move to subdirectory

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let pkgs = import <nixpkgs> {};
-    haskellPackages = pkgs.haskellPackages.override {
-      extension = self: super: {
-        midi2csv = self.callPackage ./. {};
-      };
-    };
- in pkgs.lib.overrideDerivation haskellPackages.midi2csv (attrs: {
-   buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
- })

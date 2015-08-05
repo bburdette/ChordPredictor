65dd8d32410b5e1cbd27d6752b715006400bc53b
Henry Cooney <hacoo36@gmail.com>
2015-08-05 14:18:27 -0700

    Updated csv2midi.py to allow user to specify output file

--- csv2midi/csv2midi.py
+++ csv2midi/csv2midi.py
@@ -14,8 +14,8 @@ import sys
 
 def main(argc, argv):
 
-    if argc != 2:
-        print("Usage: python csv2midi.py <csv file>")
+    if argc != 3:
+        print("Usage: python csv2midi.py <csv file> <output file name>")
         exit(1)
 
     # Pattern is the midi file. It can contain multiple tracks,
@@ -26,24 +26,28 @@ def main(argc, argv):
     
     print(argv[1])
     csvfile = open(argv[1], 'rb')
+    counter = 0
     for row in csvfile:
+        print(counter)
         vals = [x.strip() for x in row.split(',')]
+        print(vals)
+        vals = filter(None, vals)
         vals = map(lambda x: int(x), vals)
-        print vals
         tick = vals[0]
-        base = vals[1]
+        base = vals[1] + 60
+        print(len(vals))
         offsets = vals[2:]
         for i in range(0, len(offsets)):
             offsets[i] = int(offsets[i]) + base
 
         
         
-        print tick
-        print base
-        print offsets
+        #print tick
+        #print base
+        #print offsets
         data = []
         
-        track.append(midi.NoteOnEvent(tick=10, channel=1, data=[base] + [110]))
+        track.append(midi.NoteOnEvent(tick=12, channel=1, data=[base] + [110]))
         for x in offsets:
             track.append(midi.NoteOnEvent(tick=0, channel=1, data=[x]+[110]))
 
@@ -51,6 +55,7 @@ def main(argc, argv):
         for x in offsets:
             track.append(midi.NoteOffEvent(tick=0, channel=1, data=[x]+[0]))
 
+        counter += 10
 
 
     # End of track appears 1 tick after last event
@@ -58,13 +63,13 @@ def main(argc, argv):
     print("###########################################")
     print("ORIGINAL")
     print("###########################################")
-    pattern2 = midi.read_midifile("vexation.mid")
-    print(pattern2)
-    print("###########################################")
-    print("RECOVERED")
-    print("###########################################")
-    print pattern
+    #pattern2 = midi.read_midifile("vexation.mid")
+    #print(pattern2)
+    #print("###########################################")
+    #print("RECOVERED")
+    #print("###########################################")
+    #print pattern
 
-    midi.write_midifile("output.mid", pattern)
+    midi.write_midifile(argv[2], pattern)
 
 if __name__ == "__main__": main(len(sys.argv), sys.argv)
